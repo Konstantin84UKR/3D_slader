26cf9262ba5148e137d7bf462e11abf413400122
Константин Бондаренко <k.bondarenko@argo.ua>
2021-01-15 18:20:12 +0200

    исправил замечания после проверки ДЗ

--- task-4.js
+++ task-4.js
@@ -3,25 +3,34 @@ let pricePerDroid = 3000;
 let totalPrice = 0;
 let accountBalanceTemp;
 
-let result = prompt(
+
+
+while (true) {
+
+  let result = prompt(
   `Укажите количество дройдов (цена за штуку ${pricePerDroid})`
 );
+if (result == null) {
+    console.log("Отменено пользователем!");
+    alert("Отменено пользователем!");
+    break;
+}
 
 result = parseInt(result);
 if (result) {
   totalPrice = pricePerDroid * result;
   accountBalanceTemp = credits - totalPrice;
 }
+  
+  if (isNaN(result)) {
+    console.log("Введенно не число!");
+    alert("Введенно не число!");
+  } else if (accountBalanceTemp >= 0) {
+    credits = accountBalanceTemp;
+    alert(`Вы купили ${result} дроидов, на счету осталось ${credits} кредитов`);
+  } else if (accountBalanceTemp < 0) {
+    console.log("Недостаточно средств на счету!");
+    alert("Недостаточно средств на счету!");
+  }
 
-if (result == null) {
-  console.log("Отменено пользователем!");
-  //alert("Отменено пользователем!");
-} else if (isNaN(result)) {
-  console.log("Введенно не число!");
-} else if (accountBalanceTemp >= 0) {
-  credits = accountBalanceTemp;
-  alert(`Вы купили ${result} дроидов, на счету осталось ${credits} кредитов`);
-} else if (accountBalanceTemp < 0) {
-  console.log("Недостаточно средств на счету!");
-  //alert("Недостаточно средств на счету!");
 }
--- task-6.js
+++ task-6.js
@@ -1,14 +1,30 @@
 let total = 0;
-let result = null;
+//let result = null;
+let result = "";
 
-do {
+
+while (result != null){
+  
   result = prompt(`Введите число, текушее значение total = ${total}`);
-  let resultNumber = Number(result);
-  if (!Number.isNaN(resultNumber)) {
-    total += resultNumber;
-  } else {
-    result = null;
-  }
-} while (result != null);
+  
+   let resultNumber = Number(result);
+   if (!Number.isNaN(resultNumber)) {
+     total += resultNumber;
+   } else {
+     result = null;
+   }
+}
 
 alert(`total = ${total}`);
+
+// do {
+//   result = prompt(`Введите число, текушее значение total = ${total}`);
+//   let resultNumber = Number(result);
+//   if (!Number.isNaN(resultNumber)) {
+//     total += resultNumber;
+//   } else {
+//     result = null;
+//   }
+// } while (result != null);
+
+// alert(`total = ${total}`);
